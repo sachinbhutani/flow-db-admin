7e2b99bdd738ca783ba78b19c411d222d0a54003
Sachin <sachin1705@gmail.com>
2015-11-06 16:00:15 -0800

    v1.1.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sach:flow-db-admin',
-  version: '1.1.4',
+  version: '1.1.5',
   // Brief, one-line summary of the package.
   summary: 'Meteor Database Admin package for use with Flow Router',
   // URL to the Git repository containing the source code for this package.
