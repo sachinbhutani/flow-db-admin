2d9a9944b2f8a666eba846091600b177de78c00c
dc <165015@qq.com>
2019-05-19 00:36:27 +0800

    fix misleading error: Unexpected Token import

--- package.js
+++ package.js
@@ -1,82 +1,86 @@
 Package.describe({
-  name: 'sach:flow-db-admin',
-  version: '1.2.0',
-  // Brief, one-line summary of the package.
-  summary: 'Meteor Database Admin package for use with Flow Router',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/sachinbhutani/flow-db-admin',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name          : 'sach:flow-db-admin',
+    version       : '1.2.0',
+    // Brief, one-line summary of the package.
+    summary       : 'Meteor Database Admin package for use with Flow Router',
+    // URL to the Git repository containing the source code for this package.
+    git           : 'https://github.com/sachinbhutani/flow-db-admin',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation : 'README.md',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api){
+    var both = [ 'client', 'server' ];
 
-  both = ['client','server']
+    api.use(
+        [
+            'coffeescript',
+            'underscore',
+            'reactive-var',
+            'meteorhacks:unblock@1.1.0',
+            'kadira:flow-router@2.12.1',
+            'kadira:blaze-layout@2.3.0',
+            'zimme:active-route@2.3.2',
+            'reywood:publish-composite@1.5.2',
+            'aldeed:collection2-core@2.0.1',
+            'aldeed:autoform@6.2.0',
+            'aldeed:template-extension@4.1.0',
+            'alanning:roles@1.2.14',
+            'raix:handlebar-helpers@0.2.5',
+            'momentjs:moment@2.18.1',
+            'aldeed:tabular@2.1.1',
+            'mfactory:admin-lte@0.0.2',
+            'tmeasday:check-npm-versions@0.3.1',
+            'check',
+            'ecmascript',
+            'es5-shim',
+            'babel-runtime',
+        ],
+        both,
+    );
 
-  api.use(
-    [
-    'coffeescript',
-    'underscore',
-    'reactive-var',
-    'meteorhacks:unblock@1.1.0',
-    'kadira:flow-router@2.12.1',
-    'kadira:blaze-layout@2.3.0',
-    'zimme:active-route@2.3.2',
-    'reywood:publish-composite@1.5.2',
-    'aldeed:collection2-core@2.0.1',
-    'aldeed:autoform@6.2.0',
-    'aldeed:template-extension@4.1.0',
-    'alanning:roles@1.2.14',
-    'raix:handlebar-helpers@0.2.5',
-    'momentjs:moment@2.18.1',
-    'aldeed:tabular@2.1.1',
-    'mfactory:admin-lte@0.0.2',
-	'tmeasday:check-npm-versions@0.3.1',
-    'check',
-	'ecmascript'
-    ],
-    both);
+    api.use([ 'less@1.0.0 || 2.5.0', 'session', 'jquery', 'templating' ], 'client');
 
-  api.use(['less@1.0.0 || 2.5.0','session','jquery','templating'],'client')
+    api.use([ 'email' ], 'server');
 
-  api.use(['email'],'server')
+    api.addFiles(
+        [
+            'lib/both/AdminDashboard.coffee',
+            'lib/both/routes.js',
+            'lib/both/utils.coffee',
+            'lib/both/collections.coffee',
+        ],
+        both,
+    );
 
-  api.add_files([
-    'lib/both/AdminDashboard.coffee',
-    'lib/both/routes.js',
-    'lib/both/utils.coffee',
-    'lib/both/startup.coffee',
-    'lib/both/collections.coffee'
-    ], both);
+    api.addFiles(
+        [
+            'lib/client/html/admin_templates.html',
+            'lib/client/html/admin_widgets.html',
+            'lib/client/html/fadmin_layouts.html',
+            'lib/client/html/admin_sidebar.html',
+            'lib/client/html/admin_header.html',
+            'lib/client/js/admin_layout.js',
+            'lib/client/js/helpers.coffee',
+            'lib/client/js/templates.coffee',
+            'lib/client/js/events.coffee',
+            'lib/client/js/slim_scroll.js',
+            'lib/client/js/autoForm.coffee',
+            'lib/client/css/admin-custom.less',
+            'lib/client/js/main.coffee',
+        ],
+        'client',
+    );
 
-  api.add_files([
-    'lib/client/html/admin_templates.html',
-    'lib/client/html/admin_widgets.html',
-    'lib/client/html/fadmin_layouts.html',
-    'lib/client/html/admin_sidebar.html',
-    'lib/client/html/admin_header.html',
-    'lib/client/js/admin_layout.js',
-    'lib/client/js/helpers.coffee',
-    'lib/client/js/templates.coffee',
-    'lib/client/js/events.coffee',
-    'lib/client/js/slim_scroll.js',
-    'lib/client/js/autoForm.coffee',
-    'lib/client/css/admin-custom.less'
-    ], 'client');
+    api.addFiles([ 'lib/server/publish.coffee', 'lib/server/methods.coffee', 'lib/server/main.coffee' ], 'server');
 
-  api.add_files([
-    'lib/server/publish.coffee',
-    'lib/server/methods.coffee'
-    ], 'server');
-
-  //api.addAssets(['lib/client/css/admin-custom.css'],'client');
-  api.export('AdminDashboard',both)
-
+    //api.addAssets(['lib/client/css/admin-custom.css'],'client');
+    api.export('AdminDashboard', both);
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('sach:flow-db-admin');
-  api.addFiles('flow-db-admin-tests.js');
+Package.onTest(function (api){
+    api.use('tinytest');
+    api.use('sach:flow-db-admin');
+    api.addFiles('flow-db-admin-tests.js');
 });
